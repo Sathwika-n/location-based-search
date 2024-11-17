ec79e11cf0586cc75194dcb49eda774f0bc75502
Rakesh-Routhu <rakeshrouthu143@gmail.com>
2024-11-17 12:10:44 -0500

    dummy push

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from controller.maps_controller import maps_controller  # Make sure this import is compatible with FastAPI
 from controller.user_controller import user_controller
@@ -19,5 +20,5 @@ app.include_router(maps_controller)
 app.include_router(user_controller)
 
 if __name__ == '__main__':
-    import uvicorn
+   
     uvicorn.run(app, host='0.0.0.0', port=8080)
